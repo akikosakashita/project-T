33de86476bd896b4005a3684486d1fbfac8cb0fe
zd3F06 <zd3sakashi@gmail.com>
2019-03-19 15:34:43 +0900

    0319学校作業

---
+++ assets/js/prodact-price.js
@@ -0,0 +1,17 @@
+$(function() {
+  $('[name="btn"]:radio').change( function() {
+    if($('[id=a]').prop('checked')){
+      $('.text').fadeOut();
+      $('.text01').fadeIn();
+    } else if ($('[id=b]').prop('checked')) {
+      $('.text').fadeOut();
+      $('.text02').fadeIn();
+    } else if ($('[id=c]').prop('checked')) {
+      $('.text').fadeOut();
+      $('.text03').fadeIn();
+    } else if ($('[id=d]').prop('checked')) {
+      $('.text').fadeOut();
+      $('.text04').fadeIn();
+    }
+  });
+});
