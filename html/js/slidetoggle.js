158517f260b26d6f6fd29892b29fdbbcca3ab600
サカシタアキコ <zd3sakashi@gmail.com>
2019-03-24 22:42:39 +0900

    0324自宅作業

---
+++ html/js/slidetoggle.js
@@ -0,0 +1,97 @@
+
+    jQuery(function(){
+        jQuery(".q1-contents, .q1-btn2").hide();
+        jQuery(".q1-btn1").on("click", function() {
+            jQuery(".q1-contents, .q1-btn1, .q1-btn2").slideToggle();
+        });
+        jQuery(".q1-btn2").on("click", function() {
+            jQuery(".q1-btn1").slideToggle(200)
+            jQuery(".q1-btn2").slideToggle(300)
+            jQuery(".q1-contents").slideToggle(300);
+        });
+    });
+
+        jQuery(function(){
+        jQuery(".q2-contents, .q2-btn2").hide();
+        jQuery(".q2-btn1").on("click", function() {
+            jQuery(".q2-contents, .q2-btn1, .q2-btn2").slideToggle();
+        });
+        jQuery(".q2-btn2").on("click", function() {
+            jQuery(".q2-btn1").slideToggle(200)
+            jQuery(".q2-btn2").slideToggle(300)
+            jQuery(".q2-contents").slideToggle(300);
+        });
+    });
+
+        jQuery(function(){
+        jQuery(".q3-contents, .q3-btn2").hide();
+        jQuery(".q3-btn1").on("click", function() {
+            jQuery(".q3-contents, .q3-btn1, .q3-btn2").slideToggle();
+        });
+        jQuery(".q3-btn2").on("click", function() {
+            jQuery(".q3-btn1").slideToggle(200)
+            jQuery(".q3-btn2").slideToggle(300)
+            jQuery(".q3-contents").slideToggle(300);
+        });
+    });
+
+        jQuery(function(){
+        jQuery(".q4-contents, .q4-btn2").hide();
+        jQuery(".q4-btn1").on("click", function() {
+            jQuery(".q4-contents, .q4-btn1, .q4-btn2").slideToggle();
+        });
+        jQuery(".q4-btn2").on("click", function() {
+            jQuery(".q4-btn1").slideToggle(200)
+            jQuery(".q4-btn2").slideToggle(300)
+            jQuery(".q4-contents").slideToggle(300);
+        });
+    });
+
+        jQuery(function(){
+        jQuery(".q5-contents, .q5-btn2").hide();
+        jQuery(".q5-btn1").on("click", function() {
+            jQuery(".q5-contents, .q5-btn1, .q5-btn2").slideToggle();
+        });
+        jQuery(".q5-btn2").on("click", function() {
+            jQuery(".q5-btn1").slideToggle(200)
+            jQuery(".q5-btn2").slideToggle(300)
+            jQuery(".q5-contents").slideToggle(300);
+        });
+    });
+
+        jQuery(function(){
+        jQuery(".q6-contents, .q6-btn2").hide();
+        jQuery(".q6-btn1").on("click", function() {
+            jQuery(".q6-contents, .q6-btn1, .q6-btn2").slideToggle();
+        });
+        jQuery(".q6-btn2").on("click", function() {
+            jQuery(".q6-btn1").slideToggle(200)
+            jQuery(".q6-btn2").slideToggle(300)
+            jQuery(".q6-contents").slideToggle(300);
+        });
+    });
+
+        jQuery(function(){
+        jQuery(".q7-contents, .q7-btn2").hide();
+        jQuery(".q7-btn1").on("click", function() {
+            jQuery(".q7-contents, .q7-btn1, .q7-btn2").slideToggle();
+        });
+        jQuery(".q7-btn2").on("click", function() {
+            jQuery(".q7-btn1").slideToggle(200)
+            jQuery(".q7-btn2").slideToggle(300)
+            jQuery(".q7-contents").slideToggle(300);
+        });
+    });
+
+        jQuery(function(){
+        jQuery(".q8-contents, .q8-btn2").hide();
+        jQuery(".q8-btn1").on("click", function() {
+            jQuery(".q8-contents, .q8-btn1, .q8-btn2").slideToggle();
+        });
+        jQuery(".q8-btn2").on("click", function() {
+            jQuery(".q8-btn1").slideToggle(200)
+            jQuery(".q8-btn2").slideToggle(300)
+            jQuery(".q8-contents").slideToggle(300);
+        });
+    });
+
